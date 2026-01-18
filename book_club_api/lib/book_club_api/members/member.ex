75e1772c5525355b020b73ee120216f81acad0ec
aog-developer <elisandil88@gmail.com>
2026-01-18 07:51:17 +0100

    Refactor Member changeset validation to separate email and joined_at logic

--- book_club_api/lib/book_club_api/members/member.ex
+++ book_club_api/lib/book_club_api/members/member.ex
@@ -16,6 +16,26 @@ defmodule BookClubApi.Members.Member do
   def changeset(member, attrs) do
     member
     |> cast(attrs, [:name, :email, :joined_at])
-    |> validate_required([:name, :email, :joined_at])
+    |> validate_required([:name, :email])
+    |> validate_length(:name, min: 2, max: 100)
+    |> validate_email()
+    |> validate_joined_at()
+    |> unique_constraint(:email)
+  end
+
+  defp validate_email(changeset) do
+    changeset
+    |> validate_format(:email, ~r/^[\w._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, message: "is not a valid email")
+    |> validate_length(:email, min: 5, max: 255)
+  end
+
+  defp validate_joined_at(changeset) do
+    validate_change(changeset, :joined_at, fn :joined_at, joined_at ->
+      if joined_at && Date.compare(joined_at, Date.utc_today()) == :gt do
+        [joined_at: "impossible to join in a future date"]
+      else
+        []
+      end
+    end)
   end
 end
