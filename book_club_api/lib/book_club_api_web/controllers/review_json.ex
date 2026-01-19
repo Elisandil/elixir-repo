7e8aca5e447bc4c61370611cd0ba6e80bb22fd9d
aog-developer <elisandil88@gmail.com>
2026-01-19 15:39:43 +0100

    Add book and member data rendering to review JSON responses

--- book_club_api/lib/book_club_api_web/controllers/review_json.ex
+++ book_club_api/lib/book_club_api_web/controllers/review_json.ex
@@ -1,5 +1,7 @@
 defmodule BookClubApiWeb.ReviewJSON do
   alias BookClubApi.Reviews.Review
+  alias BookClubApi.Books.Book
+  alias BookClubApi.Members.Member
 
   @doc """
   Renders a list of reviews.
@@ -19,7 +21,29 @@ defmodule BookClubApiWeb.ReviewJSON do
     %{
       id: review.id,
       rating: review.rating,
-      comment: review.comment
+      comment: review.comment,
+      book: book_data(review.book),
+      member: member_data(review.member),
+      inserted_at: review.inserted_at
     }
   end
+
+  defp book_data(%Book{} = book) do
+    %{
+      id: book.id,
+      title: book.title,
+      author: book.author
+    }
+  end
+
+  defp book_data(_), do: nil
+
+  defp member_data(%Member{} = member) do
+    %{
+      id: member.id,
+      name: member.name
+    }
+  end
+
+  defp member_data(_), do: nil
 end
